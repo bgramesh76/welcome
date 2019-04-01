cf13c4824d9288a8f8e738864066a70b535a1e02
bgramesh76 <venkatarameshkumar76@gmail.com>
2019-04-01 17:07:39 +0530

    add a python

---
+++ python/python.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+
+def ram():
+    print("Hello guru")
+if __name__ == "__main__":
+    ram()
+
+
+print("welcome")
+
---
+++ python/python4.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python
+
+# var = ("Hello World!")
+
+x="Guru"
+print x[1]
+print x[1:3]
+print "u" in x
+print "u" not in x
+
+name = 'guru'
+number = 99
+print'%s %d' %(name,number)
+
+x="Guru"
+y="99"
+print x+y
+print x*2
+
+print("==============================================================")
+
+x = ("Ramesh Kumar")
+print(x[:6])
+print(x[0:6] + "Guru99")
+
+
+os="I Love Myself"
+ns=os.replace("Love", "Hate")
+print(ns)
+
+print("===================================================================")
+
+string = "python is good"
+print(string.upper())
+print(string.lower())
+print(string.capitalize())
+
+print(":".join("Python"))
+
+string="12345"
+print(' '.join(reversed(string)))
+
+word = "guru welrcome yoru"
+print(word.split())
+print(word.split('r'))
+
+print("=============================================================================")
+
+x = "Guru99"
+x.replace("Guru99","Python")
+print(x)
+
+print("===========================================================================")
+
+var1 = "Guru99!"
+var2 = "Software Testing"
+print ("var1[0]:",var1[0])
+print ("var2[1:5]:",var2[1:5])
